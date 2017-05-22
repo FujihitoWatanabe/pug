99e06a045d00c8a6b17574d9ac5129db89cf249d
DESKTOP-AIF8KO0\fuzih <fuzihito26@hotmailcom>
2017-05-22 22:31:44 +0900

    ヘッダーなどのスタイルをクエリをネストする形に変更。

--- public_html/gulpfile.js
+++ public_html/gulpfile.js
@@ -2,23 +2,24 @@
 
 /*-------------------- インストールコマンド --------------------- */
 //グローバル
-    // npm i -g gulp
+  // npm i -g gulp
 //以下はプロジェクトフォルダで入力
-    // npm i -D gulp
-    // npm i -D gulp-autoprefixer
-    // npm i -D gulp-minify-css
-    // npm i -D gulp-uglify
-    // npm i -D gulp gulp-sass gulp-postcss postcss-cssnext
-    // npm i -D browser-sync
-    // npm i -D gulp-debug
-    // npm i -D gulp-plumber
-    // npm i -D gulp-notify
-    // npm i -D gulp-imagemin
-    // npm i -D gulp-ejs
-    // npm i -D gulp-minify-ejs
-    // npm i -D gulp-rename
-    // npm i -D gulp-pug
-    // npm i -D gulp-data
+  // npm i -D gulp
+  // npm i -D gulp-autoprefixer
+  // npm i -D gulp-minify-css
+  // npm i -D gulp-uglify
+  // npm i -D gulp gulp-sass gulp-postcss postcss-cssnext
+  // npm i -D gulp-sass-glob
+  // npm i -D browser-sync
+  // npm i -D gulp-debug
+  // npm i -D gulp-plumber
+  // npm i -D gulp-notify
+  // npm i -D gulp-imagemin
+  // npm i -D gulp-ejs
+  // npm i -D gulp-minify-ejs
+  // npm i -D gulp-rename
+  // npm i -D gulp-pug
+  // npm i -D gulp-data
 /*-------------------- /インストールコマンド -------------------- */
 
 /*-------------------- plug-in -------------------------------- */
@@ -27,6 +28,7 @@ var gulp = require("gulp");
 var minifycss = require("gulp-minify-css");
 var uglify = require("gulp-uglify");
 var sass = require('gulp-sass');
+var sassGlob = require("gulp-sass-glob");
 var postcss = require("gulp-postcss");
 var cssnext = require("postcss-cssnext");
 var browserSync = require("browser-sync");
@@ -48,12 +50,12 @@ var data = require("gulp-data");
 //ベンダープレフィックス
 /*
 gulp.task("autoprefixer", function () {
-    return gulp.src("*.css")
-    .pipe(autoprefixer({
-        //IEは9以上、Androidは4以上、iOS Safariは8以上
-        //その他は最新2バージョンで必要なベンダープレフィックスを付与する
-        browsers: ["last 2 versions", "ie >= 9", "Android >= 4","ios_saf >= 8"],
-        cascade: false
+  return gulp.src("*.css")
+  .pipe(autoprefixer({
+  //IEは9以上、Androidは4以上、iOS Safariは8以上
+  //その他は最新2バージョンで必要なベンダープレフィックスを付与する
+  browsers: ["last 2 versions", "ie >= 9", "Android >= 4","ios_saf >= 8"],
+  cascade: false
 }))
 .pipe(gulp.dest("dest/css/"));
 });
@@ -61,101 +63,91 @@ gulp.task("autoprefixer", function () {
 
 //scssをコンパイル(圧縮、ベンダープレフィックス)
 var paths = {
-    "scss": "src/scss/", //作業するscssのフォルダ
-    "css": "dest/css/"  //コンパイルして保存するcssのフォルダ
+  "scss": "src/scss/", //作業するscssのフォルダ
+  "css": "dest/css/"  //コンパイルして保存するcssのフォルダ
 }
 gulp.task('scss', function() {
   var processors = [
-     //cssnext()
-     cssnext({browsers: ["last 2 versions", "ie >= 9", "Android >= 4","ios_saf >= 8"]}) //ブラウザ/os バージョン
+   //cssnext()
+   cssnext({browsers: ["last 2 versions", "ie >= 9", "Android >= 4","ios_saf >= 8"]}) //ブラウザ/os バージョン
  ];
   return gulp.src(paths.scss + "**/*.scss")
-    .pipe(postcss(processors))
-    .pipe(sass({outputStyle: "expanded"}))
-    .pipe(gulp.dest(paths.css))
+  .pipe(postcss(processors))
+  .pipe(sass({outputStyle: "expanded"}))
+  .pipe(gulp.dest(paths.css))
 });
 
 //CSS圧縮
 /*
 gulp.task("minify-css", function() {
-    return gulp.src("*.css")
-    .pipe(minifycss())
-    .pipe(gulp.dest("dest/css/"))
+  return gulp.src("*.css")
+  .pipe(minifycss())
+  .pipe(gulp.dest("dest/css/"))
 });
 */
 
 //画像圧縮
-/*gulp.task('imagemin', function(){
-    gulp.src("src/images/*.jpg")
-      .pipe(imagemin())
-      .pipe(gulp.dest("dest/images/"));
-    gulp.src("src/images/*.png")
-      .pipe(imagemin())
-      .pipe(gulp.dest("dest/images/"));
-});
-*/
 gulp.task('imagemin', function(){
   var srcGlob = 'src/images/**/*.+(jpg|jpeg|png|gif|svg)';
   var dstGlob = 'src/images/';
-
   var imageminOptions = {
-    optimizationLevel: 7
+  optimizationLevel: 7
   };
   gulp.src(srcGlob )
-    .pipe(imagemin( imageminOptions ))
-    .pipe(gulp.dest("dest/images/"));
+  .pipe(imagemin( imageminOptions ))
+  .pipe(gulp.dest("dest/images/"));
 });
 
 //JS圧縮
 gulp.task("uglify", function() {
-    return gulp.src("src/js/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest("dest/js/"))
+  return gulp.src("src/js/*.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("dest/js/"))
 });
 
 //Pug(テンプレートエンジン)
 gulp.task("pug", function () {
-    //var json = JSON.parse(fs.readFileSync("./pages.json"));
-    gulp.src(
-      ["src/pug/**/*.pug",'!' + "src/pug/**/_*.pug"] //参照するディレクトリ、出力を除外するファイル
-    )
-    .pipe(plumber())
-    .pipe(data( file => {
-        return JSON.parse(fs.readFileSync(`./pages.json`));
-    }))
-    .pipe(pug({
-      pretty:'    '
-    }))
-    .pipe(gulp.dest("dest/")) //出力先
+  //var json = JSON.parse(fs.readFileSync("./pages.json"));
+  gulp.src(
+    ["src/pug/**/*.pug",'!' + "src/pug/**/_*.pug"] //参照するディレクトリ、出力を除外するファイル
+  )
+  .pipe(plumber())
+  .pipe(data( file => {
+    return JSON.parse(fs.readFileSync(`./pages.json`));
+  }))
+  .pipe(pug({
+    pretty:'  '
+  }))
+  .pipe(gulp.dest("dest/")) //出力先
 });
 
 //EJS(テンプレートエンジン)
 //gulp.task("ejs", function() {
-//    var json = JSON.parse(fs.readFileSync("./pages.json"));
-//    gulp.src(
-//       ["src/ejs/**/*.ejs",'!' + "src/ejs/**/_*.ejs"] //参照するディレクトリ、出力を除外するファイル
-//    )
-//    .pipe(ejs(json))
-    //.pipe(ejs())
-//    .pipe(minifyejs())　//圧縮
-//    .pipe(rename({extname: ".html"})) //拡張子をhtmlに
-//    .pipe(gulp.dest("dest/")) //出力先
+//  var json = JSON.parse(fs.readFileSync("./pages.json"));
+//  gulp.src(
+//    ["src/ejs/**/*.ejs",'!' + "src/ejs/**/_*.ejs"] //参照するディレクトリ、出力を除外するファイル
+//  )
+//  .pipe(ejs(json))
+//  .pipe(ejs())
+//  .pipe(minifyejs())　//圧縮
+//  .pipe(rename({extname: ".html"})) //拡張子をhtmlに
+//  .pipe(gulp.dest("dest/")) //出力先
 //});
 
 /*-------------------- /タスク ------------------------------- */
 
 /*-------------------- リアルタイム監視------------------------ */
 gulp.task("watch", function() {
-    //gulp.watch(".*css", ["minify-css"]);
-    //gulp.watch(".*css", ["autoprefixer"]);
-    gulp.watch("src/scss/*.scss", ["scss"]);
-    gulp.watch("src/js/*.js", ["uglify"]);
-    gulp.watch("src/pug/**/*.pug", ["pug"]);
-    //gulp.watch("src/ejs/**/*.ejs", ["ejs"]);
-    browserSync.init({
-        files: ["src/scss/","src/js/","src/pug/"],
-        proxy: "http://localhost/~pug-dev/dest/",
-        //open: "external"
-    });
+  //gulp.watch(".*css", ["minify-css"]);
+  //gulp.watch(".*css", ["autoprefixer"]);
+  gulp.watch("src/scss/*.scss", ["scss"]);
+  gulp.watch("src/js/*.js", ["uglify"]);
+  gulp.watch("src/pug/**/*.pug", ["pug"]);
+  //gulp.watch("src/ejs/**/*.ejs", ["ejs"]);
+  browserSync.init({
+    files: ["src/scss/","src/js/","src/pug/"],
+    proxy: "http://localhost/~pug-dev/dest/",
+    //open: "external"
+  });
 });
 /*-------------------- /リアルタイム監視 ---------------------- */
